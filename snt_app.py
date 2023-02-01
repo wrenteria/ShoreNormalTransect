8d78672396a4408e55134e910be7e3acc2f541fd
WILLINGTON RENTERIA <wjrenteria@gmail.com>
2023-02-01 10:40:51 -0800

    Update snt_app.py

--- snt_app.py
+++ snt_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import os
-os.system('python ShoreNormalTransect')
+os.system('python ShoreNormalTransect.py')
 st.title("Shore Normal Transect")
 st.image('shorenormaltransect.png')
